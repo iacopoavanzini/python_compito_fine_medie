1ec3c07fcd3a9b3df4ac55f0534fc3fc51b82b90
Iacopo Avanzini <iacopo.avanzini@gmail.com>
2026-05-20 22:31:40 +0200

    creato metodo "get_compiti" + terminato  salvataggio su database

--- SitoFlask/DB_Operations.py
+++ SitoFlask/DB_Operations.py
@@ -1,13 +1,41 @@
 import sqlite3
+import datetime
+def get_compiti():
+    connection = sqlite3.connect("compiti.db")
+    cursor = connection.cursor()
+    cursor.execute("""CREATE TABLE IF NOT EXISTS "compiti" (
+	"id"	INTEGER,
+	"descrizione"	TEXT,
+	"titolo"	TEXT,
+    "materia"    TEXT,
+	"data_scadenza"	DATETIME,
+	"creation_date"	DATETIME,
+	"update_date"	DATETIME default current_timestamp,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);""")
+    cursor.execute("""SELECT id, descrizione, titolo, data_scadenza FROM COMPITI;""")
+    rows = cursor.fetchall()
+    return rows
 
 
 #inserting data to db
-def add_text(title, description):
+def add_text(title, description, data_scadenza, materia):
     #database connection
     # print("text_value = " +text_value)
     connection = sqlite3.connect("compiti.db")
     cursor = connection.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS compiti (id	INTEGER,descrizione	TEXT,titolo	TEXT,PRIMARY KEY(id AUTOINCREMENT));")
-    cursor.execute("INSERT INTO compiti(titolo, descrizione) VALUES (?,?)", (title, description))
+    cursor.execute("""CREATE TABLE IF NOT EXISTS "compiti" (
+	"id"	INTEGER,
+	"descrizione"	TEXT,
+	"titolo"	TEXT,
+    "materia"    TEXT,
+	"data_scadenza"	DATETIME,
+	"creation_date"	DATETIME,
+	"update_date"	DATETIME default current_timestamp,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);""")
+    cursor.execute("""INSERT INTO compiti(titolo, descrizione, data_scadenza, materia, creation_date) 
+                   VALUES (?,?,?,?,?)"""
+                   , (title, description, data_scadenza, materia, datetime.datetime.now()))
     connection.commit()
     return 1
--- SitoFlask/app.py
+++ SitoFlask/app.py
@@ -1,23 +1,25 @@
 from flask import Flask, render_template, request, redirect, url_for
-from DB_Operations import add_text
+from DB_Operations import add_text, get_compiti
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return render_template('index.html' , text= "hello world 1")
+    compiti = get_compiti()
+    for row in compiti:
+        print(row)
+    return render_template('index.html' , tasks=compiti)
+
 @app .route("/compiti" , methods=["GET"]) 
 def compiti(): 
-    memory_file = open("compiti.txt", "r", encoding = "utf8")
-    contenuto_file = memory_file.read()
-    memory_file.close()
-    return contenuto_file
-
+    return get_compiti()
 @app.route("/compiti", methods=["POST"]) 
 def  AddText(): 
     if request.method == "POST" : 
         title = request.form["title"] 
         description = request.form["description"]
+        materia = request.form["materia"]
+        data_scadenza = request.form["scadenza"]
         #salvataggio di tutti i valori nel db
-        add_new = add_text(title, description) 
+        add_new = add_text(title, description, data_scadenza, materia) 
         return redirect("/") 
 
 if __name__ == '__main__':
