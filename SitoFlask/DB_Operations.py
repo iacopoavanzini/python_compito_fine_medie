9ddc3ff4c9413b6b0575273d97c1b8a89b3d8b84
Iacopo Avanzini <iacopo.avanzini@gmail.com>
2026-05-21 22:08:14 +0200

    cancellazione dei compiti

--- SitoFlask/DB_Operations.py
+++ SitoFlask/DB_Operations.py
@@ -20,7 +20,7 @@ def get_compiti():
 
 
 #inserting data to db
-def add_text(title, description, data_scadenza, materia):
+def add_compiti(title, description, data_scadenza, materia):
     #database connection
     # print("text_value = " +text_value)
     connection = sqlite3.connect("compiti.db")
@@ -38,5 +38,23 @@ def add_text(title, description, data_scadenza, materia):
     cursor.execute("""INSERT INTO compiti(titolo, descrizione, data_scadenza, materia, creation_date) 
                    VALUES (?,?,?,?,?)"""
                    , (title, description, data_scadenza, materia, datetime.datetime.now()))
+    connection.commit()
+    return 1
+
+def delete_compiti(id):
+    connection = sqlite3.connect("compiti.db")
+    cursor = connection.cursor()
+    cursor.execute("""CREATE TABLE IF NOT EXISTS "compiti" (
+	"id"	INTEGER,
+	"descrizione"	TEXT,
+	"titolo"	TEXT,
+    "materia"    TEXT,
+	"data_scadenza"	DATETIME,
+	"creation_date"	DATETIME,
+	"update_date"	DATETIME default current_timestamp,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);""")
+    cursor.execute("""DELETE FROM compiti WHERE id = ? """
+                   , id,)
     connection.commit()
     return 1
--- SitoFlask/app.py
+++ SitoFlask/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for
-from DB_Operations import add_text, get_compiti
+from flask import Flask, render_template, request, redirect
+from DB_Operations import add_compiti, get_compiti, delete_compiti
 app = Flask(__name__)
 @app.route('/')
 def home():
@@ -12,6 +12,14 @@ def home():
 def compiti(): 
     return get_compiti()
 
+@app .route("/delete" , methods=["POST"])
+def delete():
+    if request.method == "POST"  :
+        id = request.form["id_compiti"]
+        delete_compiti(id)
+        compiti = get_compiti()
+        return render_template('index.html', compiti =compiti)
+
 @app.route("/compiti", methods=["POST"]) 
 def  AddText(): 
     if request.method == "POST" : 
@@ -20,7 +28,7 @@ def  AddText():
         materia = request.form["materia"]
         data_scadenza = request.form["scadenza"]
         #salvataggio di tutti i valori nel db
-        add_new = add_text(title, description, data_scadenza, materia) 
+        add_new = add_compiti(title, description, data_scadenza, materia) 
         return redirect("/") 
 
 if __name__ == '__main__':
